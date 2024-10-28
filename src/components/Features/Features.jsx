3a4b9b2f3191667b62878d2d978d2873c95d3250
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-28 08:56:52 +0600

    card design is complete

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
-
+import { CiCircleCheck } from "react-icons/ci";
 const Features = ({ feature }) => {
   return (
     <div className="">
-      <h3 className="font-medium text-sm">{feature}</h3>
+      <div className="flex items-center gap-2 ">
+        <CiCircleCheck className="text-green-800 bg-green-400 rounded-full" />
+        <h3 className="font-medium text-sm pt-2">{feature}</h3>
+      </div>
     </div>
   );
 };
--- src/components/GymOption/GymOption.jsx
+++ src/components/GymOption/GymOption.jsx
@@ -1,17 +1,23 @@
 import PropTypes from "prop-types";
 import Features from "../Features/Features";
+
 const GymOption = ({ option }) => {
   const { name, price, features } = option;
   return (
-    <div className="bg-blue-500 px-5 py-8 rounded-xl  space-y-3">
-      <h2 className="">
-        <span className="text-5xl font-bold">{price}</span>
+    <div className="bg-blue-500 text-white px-5 py-4 rounded-xl flex flex-col ">
+      <h2 className="text-center pb-3">
+        <span className="text-6xl font-bold">{price}</span>
         <span className="text-2xl font-medium">/mon</span>
       </h2>
-      <h3 className="font-medium text-xl ">{name}</h3>
-      {features.map((feature, idx) => (
-        <Features key={idx} feature={feature}></Features>
-      ))}
+      <h3 className="font-medium text-2xl pb-6 text-center">{name}</h3>
+      <div className=" pl-5 flex-grow ">
+        {features.map((feature, idx) => (
+          <Features key={idx} feature={feature}></Features>
+        ))}
+      </div>
+      <button className="bg-green-500 py-2 font-bold rounded-lg mt-6">
+        Buy Now
+      </button>
     </div>
   );
 };
